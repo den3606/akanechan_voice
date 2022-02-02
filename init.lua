e5f4d379e94cb7822b574b3f1b17e4069cc7d7bd
den3606 <dedendendedenpunn@gmail.com>
2022-02-02 09:23:36 +0900

    fix: voiceロードの判定をplayerEntity内に格納した

--- init.lua
+++ init.lua
@@ -3,10 +3,10 @@ dofile_once("mods/akanechan_voice/files/scripts/lib/utilities.lua")
 print("akanechan_voice load")
 
 function OnPlayerSpawned(player_entity_id)
-  local loaded = (tonumber(GlobalsGetValue("akanechan_voice.loaded?", "0")) == 1)
+  local loaded = getInternalVariableValue(player_entity_id, "akanechan_voice.loaded?", "value_bool") or false
   if not loaded then
     EntityLoadToEntity("mods/akanechan_voice/files/entities/extend_player_voices.xml", player_entity_id)
-    GlobalsSetValue("akanechan_voice.loaded?", "1")
+    addNewInternalVariable(player_entity_id, "akanechan_voice.loaded?", "value_bool", true)
   end
 end
 
