d148aa53b33c0a4abbd5f310b2cda8f24d56b4f1
den3606 <dedendendedenpunn@gmail.com>
2022-02-02 09:17:47 +0900

    fix: ゲームを再ロードした際に音声が重複再生されていた問題を修正した

--- init.lua
+++ init.lua
@@ -2,11 +2,12 @@ dofile_once("mods/akanechan_voice/files/scripts/lib/utilities.lua")
 
 print("akanechan_voice load")
 
-function OnWorldInitialized()
-  local player_entity_id = getPlayerEntity()
-  print(player_entity_id)
-  print("world init")
-  EntityLoadToEntity( "mods/akanechan_voice/files/entities/extend_player_voices.xml", player_entity_id )
+function OnPlayerSpawned(player_entity_id)
+  local loaded = (tonumber(GlobalsGetValue("akanechan_voice.loaded?", "0")) == 1)
+  if not loaded then
+    EntityLoadToEntity("mods/akanechan_voice/files/entities/extend_player_voices.xml", player_entity_id)
+    GlobalsSetValue("akanechan_voice.loaded?", "1")
+  end
 end
 
 ModRegisterAudioEventMappings("mods/akanechan_voice/files/audio/GUIDs.txt")
