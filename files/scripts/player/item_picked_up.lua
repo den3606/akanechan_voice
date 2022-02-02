f5a8b8724dfb19ad948fd018dd4eb41d24709fd1
den3606 <dedendendedenpunn@gmail.com>
2022-02-03 07:53:24 +0900

    feat/refactor: 新音源の追加、音声ファイル周りの整備
    新アクション音声を追加した
    ・キック時の声
    ・杖取得時
    ・杖が取られたとき
    
    音声の待機時間を簡単に指定できるよう変更した
    WaitTimeオブジェクトに格納している

---
+++ files/scripts/player/item_picked_up.lua
@@ -0,0 +1,28 @@
+dofile_once("mods/akanechan_voice/files/scripts/lib/utilities.lua")
+dofile_once("mods/akanechan_voice/files/scripts/lib/global_prefixes.lua")
+dofile_once("mods/akanechan_voice/files/scripts/lib/wait_frame.lua")
+
+local function playPlayerPickupSound(player_entity_id)
+  GameEntityPlaySound(player_entity_id, "player/pickup/wand")
+end
+
+local function playEnemyPickupSound(player_entity_id)
+  GameEntityPlaySound(player_entity_id, "enemy/pickup/wand")
+end
+
+--------------
+-- main
+--------------
+function item_pickup(item_entity_id, pickupper_entity_id, item_name)
+  local player_entity_id = getPlayerEntity()
+  if player_entity_id == pickupper_entity_id then
+    WaitFrame:tryCall(player_entity_id, GLOBAL_PREFIXES:item_pickup(), function()
+      playPlayerPickupSound(player_entity_id)
+    end, 600)
+  else
+    WaitFrame:tryCall(player_entity_id, GLOBAL_PREFIXES:item_pickup(), function()
+      playEnemyPickupSound(player_entity_id)
+    end, 600)
+  end
+
+end
