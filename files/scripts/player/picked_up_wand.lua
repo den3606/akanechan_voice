c9e85e19f782a2b82a0c6a3317cba08af0bf5881
den3606 <dedendendedenpunn@gmail.com>
2022-07-08 23:10:40 +0900

    feat: 茜ちゃんの杖感知範囲を実装した
    だいたい画面一個分ぐらい
    closed #21

--- files/scripts/player/picked_up_wand.lua
+++ files/scripts/player/picked_up_wand.lua
@@ -14,19 +14,25 @@ function item_pickup(item_entity_id, pickupper_entity_id, item_name)
   print("pickup wand")
   local player_entity_id = getPlayerEntity()
 
-
   local is_swapped_wand = getInternalVariableValue(player_entity_id, AKANECHAN:IS_THROWED_WAND(), "value_bool")
   print("is_swapped_wand: " .. tostring(is_swapped_wand))
-  if is_swapped_wand then return end
+  if is_swapped_wand then
+    return
+  end
 
   if player_entity_id == pickupper_entity_id then
     WaitFrame:tryCall(player_entity_id, AKANECHAN:PICKED_WAND(), function()
       playPlayerPickupSound(player_entity_id)
     end, 600)
   else
-    WaitFrame:tryCall(player_entity_id, AKANECHAN:PICKED_WAND(), function()
-      playEnemyPickupSound(player_entity_id)
-    end, 600)
+    local x, y = EntityGetTransform(player_entity_id)
+    local enemies = EntityGetInRadiusWithTag(x, y, 120, "enemy")
+    for _, enemy_entity_id in ipairs(enemies) do
+      if (enemy_entity_id == pickupper_entity_id) then
+        WaitFrame:tryCall(player_entity_id, AKANECHAN:PICKED_WAND(), function()
+          playEnemyPickupSound(player_entity_id)
+        end, 600)
+      end
+    end
   end
-
 end
