505d20e5c069799ea3cdbf9473cdb1f674cfd0e2
den3606 <dedendendedenpunn@gmail.com>
2022-07-08 21:30:46 +0900

    refactor: ダメージ時の待機時間を共通で取るようにした

--- files/scripts/player/damage_received.lua
+++ files/scripts/player/damage_received.lua
@@ -2,35 +2,48 @@ dofile_once("mods/akanechan_voice/files/scripts/lib/utilities.lua")
 
 local BIG_DAMAGE = 0.8 -- 20
 
-local function playDamageSound(player_entity_id, damage)
-  local is_low_helth = math.floor(getPlayerHealth() / getPlayerMaxHealth() * 100) <= 20
-  p(math.floor(getPlayerHealth() / getPlayerMaxHealth() * 100))
-  if is_low_helth then
-    p("low_helth")
-    WaitFrame:tryCall(player_entity_id, AKANECHAN:RECEIVED_DAMAGE(), function()
-      GameEntityPlaySound(player_entity_id, "player/take_damage/low_helth")
-    end)
-  elseif damage < BIG_DAMAGE then
-    p("mild")
-    WaitFrame:tryCall(player_entity_id, AKANECHAN:RECEIVED_DAMAGE(), function()
-      GameEntityPlaySound(player_entity_id, "player/take_damage/mild")
-    end)
-  else
-    p("heavy")
-    WaitFrame:tryCall(player_entity_id, AKANECHAN:RECEIVED_DAMAGE(), function()
-      GameEntityPlaySound(player_entity_id, "player/take_damage/heavy")
-    end)
-  end
+local function playHeavyDamageSound(player_entity_id)
+  p("heavy")
+  GameEntityPlaySound(player_entity_id, "player/take_damage/heavy")
 end
 
+local function playMildDamageSound(player_entity_id)
+  p("mild")
+  GameEntityPlaySound(player_entity_id, "player/take_damage/mild")
+end
 
+local function playLowHealthDamageSound(player_entity_id)
+  p("low_helth")
+  GameEntityPlaySound(player_entity_id, "player/take_damage/low_helth")
+end
 
 local function playFireDamageSound(player_entity_id)
   WaitFrame:tryCall(player_entity_id, AKANECHAN:RECEIVED_FIRE_DAMAGE(), function()
     GameEntityPlaySound(player_entity_id, "player/take_damage/on_fire")
   end, 60 * 3)
-  WaitFrame:updateWaitFrame(player_entity_id, AKANECHAN:RECEIVED_FIRE_DAMAGE(), 60 * 3)
-  WaitFrame:updateWaitFrame(player_entity_id, AKANECHAN:RECEIVED_DAMAGE(), 60 * 2)
+end
+
+local function playDamageSound(player_entity_id, damage)
+  for _, damage_model in ipairs(EntityGetComponent(player_entity_id, "DamageModelComponent") or {}) do
+
+    local is_low_helth = math.floor(getPlayerHealth() / getPlayerMaxHealth() * 100) <= 20
+    local is_on_fire = ComponentGetValue2(damage_model, "is_on_fire")
+    local take_big_damage = BIG_DAMAGE < damage
+
+    if is_on_fire then
+      if take_big_damage then
+        playHeavyDamageSound(player_entity_id)
+      else
+        playFireDamageSound(player_entity_id)
+      end
+    elseif is_low_helth then
+      playLowHealthDamageSound(player_entity_id)
+    elseif take_big_damage then
+      playHeavyDamageSound(player_entity_id)
+    else
+      playMildDamageSound(player_entity_id)
+    end
+  end
 end
 
 --------------
@@ -41,17 +54,9 @@ function damage_received(damage, desc, entity_who_caused, is_fatal)
   if is_fatal then
     GameEntityPlaySound(player_entity_id, "player/dead")
   else
-    for _, damage_model in ipairs(EntityGetComponent(player_entity_id, "DamageModelComponent") or {}) do
-      if ComponentGetValue2(damage_model, "is_on_fire") then
-        if damage < BIG_DAMAGE then
-          playFireDamageSound(player_entity_id)
-        else
-          playDamageSound(player_entity_id, damage)
-        end
-      else
-        playDamageSound(player_entity_id, damage)
-      end
-    end
+    WaitFrame:tryCall(player_entity_id, AKANECHAN:RECEIVED_DAMAGE(), function()
+      playDamageSound(player_entity_id, damage)
+    end)
   end
 end
 
