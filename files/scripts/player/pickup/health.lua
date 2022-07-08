3126db34b60fa6bb7485e692df660ffe299efaa5
den3606 <dedendendedenpunn@gmail.com>
2022-07-08 09:39:37 +0900

    tmp: リフレッシャー/ヘルス回復時の処理

---
+++ files/scripts/player/pickup/health.lua
@@ -0,0 +1,8 @@
+dofile_once("mods/akanechan_voice/files/scripts/lib/utilities.lua")
+
+--------------
+-- main
+--------------
+function item_pickup(item_entity_id, pickupper_entity_id, item_name)
+  -- TODO: ヘルス回復時の音声追加
+end
