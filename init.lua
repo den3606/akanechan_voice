e6db49c1dda97156cf4279e3c262ace14f4ee6d5
den3606 <dedendendedenpunn@gmail.com>
2022-08-15 02:12:08 +0900

    feat/fix: 羊変化時のボイス追加を行った
    奇変形でPlayerが変身した後に
    声が聞こえなくなる問題も修正しました

--- init.lua
+++ init.lua
@@ -1,6 +1,8 @@
 dofile_once("mods/akanechan_voice/files/scripts/lib/utilities.lua")
-
 print("akanechan_voice load")
+
+local registered_entity_id = nil
+local is_player_dead = false
 
 function OnModInit()
   -- 音声ファイルロード
@@ -8,7 +10,8 @@ function OnModInit()
 end
 
 function OnPlayerSpawned(player_entity_id)
-  local player_entity_id = getPlayerEntity()
+  registered_entity_id = player_entity_id
+  is_player_dead = false
 
   SoundPlayer:destroySoundPlayer(player_entity_id, AKANECHAN.SOUND_PLAYER_NAME)
   AkanechanVoice = SoundPlayer:create(player_entity_id, AKANECHAN.SOUND_PLAYER_NAME, AKANECHAN:SOUND_FILE_STORAGE_NAME())
@@ -22,8 +25,34 @@ function OnPlayerSpawned(player_entity_id)
 end
 
 function OnWorldPreUpdate()
+  if is_player_dead then
+    return
+  end
+
+  local current_player_entity = getPlayerEntity()
+  local is_transformed_player = current_player_entity ~= registered_entity_id and current_player_entity ~= nil
+  ReregisterSoundPlayer(is_transformed_player, current_player_entity)
+
+  if is_transformed_player and FindSheepPlayer() then
+    dofile('mods/akanechan_voice/files/scripts/player/sheep.lua')
+  end
+
   if AkanechanVoice ~= nil then
     SoundPlayer:playSound(AkanechanVoice)
+  end
+
+end
+
+function OnPlayerDied()
+  is_player_dead = true
+end
+
+function ReregisterSoundPlayer(is_transformed_player, current_player_entity)
+  if is_transformed_player then
+    SoundPlayer:destroySoundPlayer(current_player_entity, AKANECHAN.SOUND_PLAYER_NAME)
+    SoundPlayer:destroySoundPlayer(registered_entity_id, AKANECHAN.SOUND_PLAYER_NAME)
+    AkanechanVoice = SoundPlayer:create(current_player_entity, AKANECHAN.SOUND_PLAYER_NAME, AKANECHAN:SOUND_FILE_STORAGE_NAME())
+    registered_entity_id = getPlayerEntity()
   end
 end
 
